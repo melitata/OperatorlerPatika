42e15d312bbb0205e7c7e57e8d1a7915573bc5fa
Melisa Kılıç <mk_i3@hotmail.com>
2024-10-27 19:35:34 +0300

    Add project files.

---
+++ ConsoleApp18/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+namespace operatorler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 3;
+            int y = 3;
+            y = y + 2;
+            Console.WriteLine(y);
+            y += 2;
+            Console.WriteLine(y);
+            y /= 1;
+            Console.WriteLine(y);
+            x *= 2;
+            Console.WriteLine(x);
+
+            //Mantıksal Operatörler
+            // ||, &&, !
+            bool isSuccess = true;
+            bool isCompleted = false;
+
+            if (isSuccess && isCompleted)
+                Console.WriteLine("Perfect!");
+
+            if (isSuccess || isCompleted)
+                Console.WriteLine("Great!");
+            if (isSuccess && !isCompleted)
+                Console.WriteLine("Fine!");
+
+
+
+        }
+    }
+
+}
